71646c4e5e1600f01b89bf02311433147469bc73
Liliam <liliam.massako@gmail.com>
2021-06-17 09:41:53 -0300

    Primeiro Arquivo

---
+++ src/test/java/features/preencheformulario.feature
@@ -0,0 +1,13 @@
+# language:pt
+  # encoding: utf-8
+
+  Funcionalidade: Se cadastrar no facebook
+
+    Cenario: Cadastro de Carros
+      Dado que estou no site
+      Quando o usuário está na tela Vehicle Data
+      E entra na pagina insurant data
+      E preenche os dados corretamente
+      E entra na pagina product data
+      E preenche price option
+      Entao send quote
---
+++ src/test/java/steps/stepsPreencheFormulario.java
@@ -0,0 +1,43 @@
+package steps;
+
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Quando;
+
+public class stepsPreencheFormulario {
+
+	@Dado("que estou no site")
+	public void que_estou_no_site() {
+		http://sampleapp.tricentis.com/101/app.php;
+	}
+
+	@Quando("o usuario esta na tela Vehicle Data")
+	public void o_usuario_esta_na_tela_vehicle_data() {
+	    
+	}
+
+	@Quando("entra na pagina insurant data")
+	public void entra_na_pagina_insurant_data() {
+	   
+	}
+
+	@Quando("preenche os dados corretamente")
+	public void preenche_os_dados_corretamente() {
+	   
+	}
+
+	@Quando("entra na pagina product data")
+	public void entra_na_pagina_product_data() {
+	    
+	}
+
+	@Quando("preenche price option")
+	public void preenche_price_option() {
+	    
+	}
+
+	@Entao("send quote")
+	public void send_quote() {
+	    
+	}
+
+}
